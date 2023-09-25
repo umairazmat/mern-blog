99fa79a42f14957afccb4241f0d2e140c92303b5
Umair Azmat <umairazmat8260.com>
2023-09-25 20:14:41 +0500

    navigation updated

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,15 +8,25 @@ export default function Navigation() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="primary" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="#" className="fw-bold">
-          MERN Blog
-        </Navbar.Brand>
+        <Link className="text-white text-decoration-none px-2" to="/">
+          <Navbar.Brand  className="fw-bold">
+            MERN Blog
+          </Navbar.Brand>
+        </Link>
+
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto"></Nav>
           <Nav>
-            <Link className="text-white text-decoration-none px-2" to="/about">About</Link>
-            <Link className="text-white text-decoration-none px-2" to="/">All Blogs</Link>
+            <Link className="text-white text-decoration-none px-2" to="/about">
+              About
+            </Link>
+            <Link
+              className="text-white text-decoration-none px-2"
+              to="/add-blog"
+            >
+              Add BLog
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
