bafaae0ee52f977f077de91943c7a5299456b6d2
Umair Azmat <umairazmat8260.com>
2023-09-25 20:14:28 +0500

    blog gallery

---
+++ src/utils/constant.js
@@ -0,0 +1,20 @@
+export const blogDetails = [
+  {
+    name: "Full Stack Developer",
+    author: "Umair Azmat",
+    description:
+      "I am a Full Stack Developer with 2 years of experience in the IT industry. I have worked on various technologies like React, Node, Express, MongoDB, MySQL, etc. I have also worked on AWS services like EC2, S3, Lambda, etc. I have also worked on various other technologies like Python, Django, Java, Spring Boot, etc. I have also worked on various other technologies like Python, Django, Java, Spring Boot, etc.",
+  },
+  {
+    name: "Frontend Developer",
+    author: "Usman Khalil",
+    description:
+      "I am a Frontend Developer with 2 years of experience in the IT industry. I have worked on various technologies like React, Node, Express, MongoDB, MySQL, etc. I have also worked on AWS services like EC2, S3, Lambda, etc. I have also worked on various other technologies like Python, Django, Java, Spring Boot, etc. I have also worked on various other technologies like Python, Django, Java, Spring Boot, etc.",
+  },
+  {
+    name: "Backend Developer",
+    author: "Noman Mazhar",
+    description:
+      "I am a Backend Developer with 2 years of experience in the IT industry. I have worked on various technologies like React, Node, Express, MongoDB, MySQL, etc. I have also worked on AWS services like EC2, S3, Lambda, etc. I have also worked on various other technologies like Python, Django, Java, Spring Boot, etc. I have also worked on various other technologies like Python, Django, Java, Spring Boot, etc. ",
+  },
+];
